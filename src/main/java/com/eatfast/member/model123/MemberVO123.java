376ce90bcda45ef4005dadb57a6074a796b1dfb5
H.Y.Fn <young19960127@gmail.com>
2025-05-29 16:23:48 +0800

    123

---
+++ src/main/java/com/eatfast/member/model123/MemberVO123.java
@@ -0,0 +1,26 @@
+package com.eatfast.member.model123;
+
+public class MemberVO123 {
+	private Integer memId;
+	private String XXX;
+	private String yyy;
+	private String zzz;
+	
+	
+	public Integer getMemId() {
+		return memId;
+	}
+	public void setMemId(Integer memId) {
+		this.memId = memId;
+	}
+	public String getXXX() {
+		return XXX;
+	}
+	public void setXXX(String xXX) {
+		XXX = xXX;
+	}
+	public MemberVO123() {
+		super();
+	}
+	
+}
